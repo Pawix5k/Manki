e8427b7330093d4a1fb7f33549ec2a147d92e757
guarando <pawelpurzyk@gmail.com>
2023-05-19 10:52:48 +0200

    Adapt main.py to prod

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from router import router
@@ -10,8 +11,21 @@ load_dotenv()
 
 if os.environ.get("MODE") == "dev":
     app = FastAPI()
+    origins = ["*"]
 else:
-    app = FastAPI(docs_url=None, redoc_url=None)   
+    app = FastAPI(docs_url=None, redoc_url=None)
+    origins = [
+        "https://manki.herokuapp.com",
+        "http://manki.herokuapp.com",
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
